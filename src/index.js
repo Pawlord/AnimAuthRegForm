1a3615ffcc855087b2ffb9ef5faf99f5dcffc2aa
Pawlord <Pavel23082002@mail.ru>
2025-01-07 20:24:58 +0300

    add speed-test vercel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SpeedInsights>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </SpeedInsights>
+
 );

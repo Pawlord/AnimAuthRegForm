57edb69b35c3c96ef876d0d0cb91b6c4189278c0
Pawlord <Pavel23082002@mail.ru>
2025-01-07 20:29:05 +0300

    delete speed-test vercel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SpeedInsights>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </SpeedInsights>
 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
